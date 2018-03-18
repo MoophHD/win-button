9bacd33151e5174535b58e9aef2e68104ec406d5
EC2 Default User <ec2-user@ip-172-31-22-156.us-east-2.compute.internal>
2018-03-18 09:27:27 +0000

    appEnry to app.json

--- AppEntry.js
+++ AppEntry.js
@@ -2,6 +2,12 @@ import React, { Component } from 'react';
 import { Provider } from 'react-redux';
 import configureStore from './src/state/configureStore';
 import App from './src/App';
+import { KeepAwake, registerRootComponent } from 'expo';
+
+if (__DEV__) {
+  KeepAwake.activate();
+}
+
 
 const store = configureStore();
 
@@ -15,3 +21,6 @@ export default class AppEntry extends Component {
     );
   }
 }
+
+
+registerRootComponent(AppEntry);
